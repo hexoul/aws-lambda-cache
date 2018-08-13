4268cae5281fac89d8f1062f3cc6da84280f15d8
hexoul <crosien@gmail.com>
2018-08-13 17:07:51 +0900

    exception handling

--- main.go
+++ main.go
@@ -19,14 +19,16 @@ func lambdaHandler(ctx context.Context, request events.APIGatewayProxyRequest) (
 	respBody := ""
 	statusCode := 200
 
-	key := request.QueryStringParameters["key"]
-	val := request.QueryStringParameters["val"]
-	if val == "" {
-		// In case of "get"
-		respBody = cacheMap[key]
-	} else if key != "" {
-		// In case of "set"
-		cacheMap[key] = val
+	if request.QueryStringParameters != nil {
+		key := request.QueryStringParameters["key"]
+		val := request.QueryStringParameters["val"]
+		if val == "" {
+			// In case of "get"
+			respBody = cacheMap[key]
+		} else if key != "" {
+			// In case of "set"
+			cacheMap[key] = val
+		}
 	}
 
 	return events.APIGatewayProxyResponse{Body: respBody, StatusCode: statusCode}, nil

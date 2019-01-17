b903d2938d1794aa1b81863543c210e0c88915f9
hexoul <crosien@gmail.com>
2019-01-17 16:18:32 +0900

    price: imp. price monitoring API powered by CMC

--- main.go
+++ main.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	cmc "github.com/hexoul/go-coinmarketcap"
+	"github.com/hexoul/go-coinmarketcap/types"
+)
+
+const (
+	cmcAPIKey    = "CMC_API_KEY"
+	targetSymbol = "BTC"
+	targetQuotes = "USD,KRW"
 )
 
 var (
-	cacheMap map[string]string
-	headers  = map[string]string{
-		"Content-Type":                "application/json",
-		"Access-Control-Allow-Origin": "*",
+	headers = map[string]string{
+		"Content-Type":                 "application/json",
+		"Access-Control-Allow-Origin":  "*",
+		"Access-Control-Allow-Headers": "*",
+		"Access-Control-Allow-Methods": "*",
 	}
+	cacheMap map[string]string
+
+	lastUpdatedPrice time.Time
+	lastPriceInfo    string
 )
 
 func init() {
 	cacheMap = make(map[string]string)
+	lastUpdatedPrice = time.Now().AddDate(0, 0, -1)
 }
 
-func lambdaHandler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	respBody := ""
-	statusCode := 200
-
+func cache(request events.APIGatewayProxyRequest) (respBody string) {
 	key := request.QueryStringParameters["key"]
-	if key == "" {
-		// Nothing to do
-	} else if request.Body != "" {
+	if request.Body != "" {
 		// In case of "set" with body
 		cacheMap[key] = request.Body
 	} else if request.QueryStringParameters != nil {
@@ -39,6 +51,39 @@ func lambdaHandler(ctx context.Context, request events.APIGatewayProxyRequest) (
 			// In case of "set"
 			cacheMap[key] = val
 		}
+	}
+	return
+}
+
+func price() (respBody string) {
+	if time.Now().Sub(lastUpdatedPrice).Minutes() < 1 {
+		return lastPriceInfo
+	}
+	if data, err := cmc.GetInstanceWithKey(cmcAPIKey).CryptoMarketQuotesLatest(&types.Options{
+		Symbol:  targetSymbol,
+		Convert: targetQuotes,
+	}); err == nil {
+		for _, v := range data.CryptoMarket {
+			if last, err := time.Parse(time.RFC3339, v.Quote["USD"].LastUpdated); err == nil {
+				lastUpdatedPrice = last
+			}
+			if b, bErr := json.Marshal(v); bErr == nil {
+				lastPriceInfo = string(b)
+			}
+			break
+		}
+	}
+	return lastPriceInfo
+}
+
+func lambdaHandler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	respBody := ""
+	statusCode := 200
+
+	if request.QueryStringParameters["price"] != "" {
+		respBody = price()
+	} else if request.QueryStringParameters["key"] != "" {
+		respBody = cache(request)
 	}
 
 	return events.APIGatewayProxyResponse{Headers: headers, Body: respBody, StatusCode: statusCode}, nil
--- main_test.go
+++ main_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
+
+	cmc "github.com/hexoul/go-coinmarketcap"
+	"github.com/hexoul/go-coinmarketcap/types"
 )
 
 func TestCache(t *testing.T) {
@@ -13,7 +18,7 @@ func TestCache(t *testing.T) {
 	event.QueryStringParameters["val"] = "123"
 	resp, err := lambdaHandler(nil, event)
 	if err != nil {
-		t.Errorf("Failed to set")
+		t.Error("Failed to set")
 	}
 
 	event = events.APIGatewayProxyRequest{}
@@ -21,7 +26,37 @@ func TestCache(t *testing.T) {
 	event.QueryStringParameters["key"] = "test"
 	resp, err = lambdaHandler(nil, event)
 	if resp.Body == "" || err != nil {
-		t.Errorf("Failed to get")
+		t.Error("Failed to get")
 	}
 	t.Log(resp.Body)
+}
+
+func TestPrice(t *testing.T) {
+	event := events.APIGatewayProxyRequest{}
+	event.QueryStringParameters = make(map[string]string)
+	event.QueryStringParameters["price"] = "BTC"
+	resp, err := lambdaHandler(nil, event)
+	if err != nil {
+		t.Error("Failed to get price")
+	}
+	t.Log(resp.Body)
+}
+
+func TestCmcAPI(t *testing.T) {
+	if data, err := cmc.GetInstanceWithKey("CMC_API_KEY").CryptoMarketQuotesLatest(&types.Options{
+		Symbol:  "BTC",
+		Convert: "USD,ETH",
+	}); err != nil {
+		t.Fatal(err)
+	} else {
+		for _, v := range data.CryptoMarket {
+			if b, bErr := json.Marshal(v); bErr == nil {
+				t.Log(string(b))
+			}
+			if last, err := time.Parse(time.RFC3339, v.Quote["USD"].LastUpdated); err == nil {
+				t.Logf("last: %s\n", last.String())
+				t.Logf("diff: %f\n", time.Now().Sub(last).Minutes())
+			}
+		}
+	}
 }
